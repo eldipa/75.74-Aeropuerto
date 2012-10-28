0d2c13b70f1c7d537316e574713c179710cca337
Di Paola Martin <martinp.dipaola@gmail.com>
2012-10-28 00:03:46 -0300

    Terminado el portado de syslog a log.

--- processes/robot_carga.cpp
+++ processes/robot_carga.cpp
@@ -11,12 +11,12 @@
 
 #include "contenedor.h"
 #include "equipaje.h"
-#include <syslog.h>
+#include "log.h"
 
 int main( int argc, char** argv ) {
    
    if (argc < 3) {
-      syslog(LOG_CRIT, "[Crit] Insuficientes parametros para robot de carga, se esperaba (id, cinta_in)\n");
+      Log::crit("Insuficientes parametros para robot de carga, se esperaba (id, cinta_in)\n");
       exit(1);
    }
 
@@ -24,29 +24,29 @@ int main( int argc, char** argv ) {
    ApiCinta cinta_in(atoi(argv[2]));
    ApiAdmin admin;
 
-   syslog(LOG_INFO, "[Info] Iniciando robot carga(%s), %s\n", argv[1], argv[2]);
+   Log::info("Iniciando robot carga(%s), %s\n", argv[1], argv[2]);
 
    for(;;) {
       sleep(rand() % 10);
-      syslog(LOG_INFO, "[Info] RobotCarga(%s) Sacando un nuevo equipaje de cinta(%s)\n", argv[1], argv[2]);
+      Log::info("RobotCarga(%s) Sacando un nuevo equipaje de cinta(%s)\n", argv[1], argv[2]);
 
       Equipaje equipaje = cinta_in.sacar_equipaje();
-      syslog(LOG_INFO, "[Info] RobotCarga(%s) se tomo el equipaje %s\n", argv[1], equipaje.toString().c_str());
+      Log::info("RobotCarga(%s) se tomo el equipaje %s\n", argv[1], equipaje.toString().c_str());
 
       std::string escala = admin.get_escala( equipaje );
       bool equipaje_en_contenedor = false;
 
       while( !equipaje_en_contenedor ) {
          sleep(rand() % 10);
-         syslog(LOG_INFO, "[Info] RobotCarga(%s) esperando contenedor para escala %s\n", argv[1], escala.c_str());
+         Log::info("RobotCarga(%s) esperando contenedor para escala %s\n", argv[1], escala.c_str());
          Contenedor contenedor = zona.esperar_contenedor(escala);
 
          if( contenedor.espacio_disponible(equipaje) ) {
-            syslog(LOG_INFO, "[Info] RobotCarga(%s) agregando equipaje.\n", argv[1]);
+            Log::info("RobotCarga(%s) agregando equipaje.\n", argv[1]);
             contenedor.agregar_equipaje(equipaje);
             equipaje_en_contenedor = true;
          } else {
-            syslog(LOG_INFO, "[Info] RobotCarga(%s) retirando contenedor lleno.\n", argv[1]);
+            Log::info("RobotCarga(%s) retirando contenedor lleno.\n", argv[1]);
             zona.retirar_contenedor(escala);
          }         
       }
