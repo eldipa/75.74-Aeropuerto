53128c45223cf9fa80e2b6684e4da42f669468f7
Di Paola Martin <martinp.dipaola@gmail.com>
2012-10-31 18:29:46 -0300

    Agregada la documentacion faltante a todas las interfaces de la base de datos.

--- db/tupleiter.h
+++ db/tupleiter.h
@@ -27,9 +27,44 @@ class TupleIterator {
 
       bool operator!=(const TupleIterator& t);
       TupleIterator& operator++();
-      
+    
+      /*
+       * This method return the number of columns of each tuple iterated.
+       * */     
       int size() const;
-
+        
+      /*
+       * With this method, the iterator can access to one column and interpret it according the template argument.
+       * For example, to read the second column which is an integer you should execute:
+       *    it.at<int>(1)
+       * 
+       * As you can see, the first column has the index 0.
+       * The specific types that you can use are in Element:: namespaces and are:
+       *    Element::integer (or int)
+       *    Element::real    (or double)
+       *    Element::text    (or const unsigned char*)
+       *    Element::blob    (or const void*)
+       * 
+       * Other type may cause an exception in runtime.
+       *
+       * If you use the wrong type, the underlying engine will perform a conversion.
+       *    An Element::  | when is readed as an Element::   | then you will obtain
+       *      integer     |              real                | the real version (double) of the integer (int)
+       *  integer or real |           text or blob           | the same number but as a string like "12"
+       *       real       |             integer              | the number truncated
+       *   text or blob   |         integer or real          | the conversion of the string using atoi/atof (beware!)
+       *       text       |              blob                | no change
+       *       blob       |              text                | add a nul character at the end if it is needed
+       *
+       * If the readed value is NULL this will be interpreted as a 0 (for Element::integer and Element::real) or as a null pointer
+       * (for Element::text and Element::blob)
+       *
+       * The types Element::text and Element::blob are pointers to an area that is managed by the internal engine. 
+       * You must not use free(), delete, or any like that.
+       *
+       * It is responsibility of the upper layers know the size of the Element::text and the Element::blob. There is
+       * no way to obtain that sizes.
+       * */
       template<typename T> T at(int i) {
          check(i);
          return Element::_private::E<T>(stmt, i);
