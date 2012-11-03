4945b8ddb7076774b560775233bba27bdbea1cff
Juan Manuel Barreneche <snipperme@gmail.com>
2012-11-03 16:56:22 -0300

    Change main to use process to spawn robots

--- processes/main.cpp
+++ processes/main.cpp
@@ -1,8 +1,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <signal.h>
 
 #include "log.h"
+#include "process.h"
 #include "equipaje.h"
 #include "constants.h"
 
@@ -24,8 +26,6 @@ char *args_puesto_checkin[] = { (char*) "puesto_checkin",
  **/
 
 int main() {
-	pid_t pid = 0;
-
   CintaCheckin checkin_to_scanner(TOSTRING(MAKE_PATH) "/mem.txt",
                                   TOSTRING(MAKE_PATH) "/sem.txt",
                                   true);
@@ -33,23 +33,17 @@ int main() {
 
   Log::info("iniciando simulación...");
 
-  Log::info("Lanzo proceso puesto_checkin(%d)...", pid);
-
-	if ( (pid = fork()) == 0 ) {
-		if( execv( "puesto_checkin", args_puesto_checkin ) == -1 ) {
-			perror("error:");
-	 		exit(0);
-		}
-	}
-
-  Log::info("Lanzo proceso robot_checkin(%d)...", pid);
-	if ( (pid = fork()) == 0 ) {
-		if( execv( "robot_checkin", args_robot_checkin ) == -1 ) {
-			perror("error:");
-			exit(0);
-		}
-	}
+  Process puesto_checkin("puesto_checkin", args_puesto_checkin);
+  Process robot_checkin("robot_checkin", args_robot_checkin);
    	
   sleep( TIEMPO_SIMULACION );
+
+  Log::info("finalizando robots...");
+
+  puesto_checkin.send_signal(SIGTERM, false);
+  robot_checkin.send_signal(SIGTERM, false);
+
+  Log::info("finalizando simulación...");
+
   return 0;
 }
