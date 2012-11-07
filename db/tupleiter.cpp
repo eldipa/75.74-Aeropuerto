514516c81215bfbb011f143724e3f52937a78b86
Di Paola Martin <martinp.dipaola@gmail.com>
2012-11-07 17:09:57 -0300

    Reverteados parcialmente los cambios introducidos en el commit 000c490c82bdc9323bf75f3370fa7d25f8a0f000. Solo se revertearon las clases Statement, TupleIterator, SemaphoreSet. Tambien se cobro el archivo yasper en el modulo external. Tambien se elimino del repositorio el binario robot_scanner. Por favor NO editen la indentacion de files ya commiteados y NO commiteen binarios salvo que sea necesario.

--- db/tupleiter.cpp
+++ db/tupleiter.cpp
@@ -1,3 +1,4 @@
+
 #include <sqlite3.h>
 #include <unistd.h>
 #include "valueerror.h"
@@ -5,47 +6,47 @@
 #include "tupleiter.h"
 #include "log.h"
 
+
 void TupleIterator::check(int i) const {
-	i = 0;
-	if (i < 0 or i >= size())
-		throw ValueError("Out of range [%i,%i). Index used was %i.", 0, size(), i);
+   if(i < 0 or i >= size())
+      throw ValueError("Out of range [%i,%i). Index used was %i.", 0, size(), i);
 }
 
 int TupleIterator::size() const {
-	return sqlite3_column_count(&stmt);
+   return sqlite3_column_count(&stmt);
 }
 
-TupleIterator::TupleIterator(sqlite3 &db, sqlite3_stmt &stmt, bool done) :
-		db(db), stmt(stmt), done(done), reset(not done) {
-	if (not done) {
-		++(*this); //Set the iterator to point the first element (if any)
-	}
+
+TupleIterator::TupleIterator(sqlite3 &db, sqlite3_stmt &stmt, bool done) : db(db), stmt(stmt), done(done), reset(not done) {
+   if(not done) {
+      ++(*this);  //Set the iterator to point the first element (if any)
+   }
 }
 
 bool TupleIterator::operator!=(const TupleIterator& t) {
-	return this->done != t.done;
+   return this->done != t.done;
 }
 
 TupleIterator& TupleIterator::operator++() {
-	while (true) {
-		switch (sqlite3_step(&stmt)) {
-		case SQLITE_DONE:
-			done = true;
-		case SQLITE_ROW:
-			return *this;
-		case SQLITE_BUSY:
-			sleep(1);
-			break;
-		default:
-			throw DBError(db, "An error ocurred when executing the statement.");
-		}
-	}
+   while(true) {
+      switch(sqlite3_step(&stmt)) {
+         case SQLITE_DONE:
+            done = true;
+         case SQLITE_ROW:
+            return *this;
+         case SQLITE_BUSY:
+            sleep(1);
+            break;
+         default:
+            throw DBError(db, "An error ocurred when executing the statement.");
+      }
+   }
 }
 
-TupleIterator::~TupleIterator() throw () {
-	if (reset)
-		if (sqlite3_reset(&stmt) != SQLITE_OK)
-			Log::crit("An exception happend during the course of a destructor:\n%s",
-					DBError(db, "The statement cannot be reset (or initializated).").what());
+
+TupleIterator::~TupleIterator() throw() {
+   if(reset)
+      if(sqlite3_reset(&stmt) != SQLITE_OK)
+         Log::crit("An exception happend during the course of a destructor:\n%s", DBError(db, "The statement cannot be reset (or initializated).").what());
 }
 
