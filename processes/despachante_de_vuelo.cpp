6b5783a620a560b5aedccc83393848e6f183fb7b
raulopez <raulopez0@gmail.com>
2012-11-12 12:28:16 -0300

    agrego procesod espachante de vuelo

---
+++ processes/despachante_de_vuelo.cpp
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+
+#include "log.h"
+#include "api_checkin.h"
+#include "api_despacho_vuelo.h"
+
+
+int get_zona_asignada(int num_vuelo);
+int tomar_zona(int num_zona);
+int get_duracion_checkin(int num_vuelo);
+
+class VueloNoRegistradoException {
+public:
+   VueloNoRegistradoException(int num_vuelo):num_vuelo(num_vuelo) {
+   }
+private:
+   int num_vuelo;
+};
+
+int main(int argc, char** argv) {
+   if (argc < 5) {
+      Log::crit("Insuficientes parametros para despachante de vuelo (num_vuelo path_vuelos path_cinta_checkin, id_checkin)\n");
+      return (1);
+   }
+
+   int num_vuelo = atoi(argv[1]);
+   int zona_asignada, zona_utilizada,  duracion_checkin;
+   ApiDespachoVuelo api_vuelo(argv[2], num_vuelo);
+
+   try {
+      
+      Log::info("DespachanteVuelos(%d) Iniciando despachante de vuelos...", num_vuelo);
+
+      //activo robot_despacho
+      zona_asignada = get_zona_asignada(num_vuelo);
+      zona_utilizada = tomar_zona(zona_asignada);
+      Log::info("DespachanteVuelos(%d) robot de despacho empieza a recibir equipajes para zona %d", num_vuelo, zona_utilizada);
+      
+      //activo robot_carga
+      Log::info("DespachanteVuelos(%d) activo robot_carga (TODO!!!) de zona%d", num_vuelo, zona_utilizada);
+
+      //abro checkin
+      ApiCheckIn api_checkin(0, argv[2], 0);
+      api_checkin.iniciar_checkin(num_vuelo);
+      Log::info("DespachanteVuelos(%d) incicio checkin puesto %d", num_vuelo, 0);
+
+      //espero cierre checkin
+      duracion_checkin = get_duracion_checkin(num_vuelo);
+      sleep(duracion_checkin);
+      api_checkin.cerrar_checkin();
+      Log::info("DespachanteVuelos(%d) cierro checkin %d", num_vuelo);
+
+      //espero llegada vuelos intercargo
+      //espero fin de carga de equipajes
+      //espero llegada avion
+      Log::info("DespachanteVuelos(%d) esperando intercargo, carga de equipajes y llegada del avion...");
+      
+      bool fin_intercargo, fin_carga, llego_avion;
+      fin_intercargo = fin_carga = llego_avion = false;
+
+      while ( (!fin_intercargo) || (!fin_carga) || (!llego_avion) ) {
+         tEventoVuelo ev = api_vuelo.recibir_evento();
+
+         switch( ev ) {
+         case FIN_INTERCARGO:
+            Log::info("DespachanteVuelos(%d) llegaron todos los vuelos de intercargo!!!", num_vuelo);
+            fin_intercargo = true;
+            break;
+         case FIN_CARGA:
+            Log::info("DespachanteVuelos(%d) termino la carga del equipaje en el avion!!!", num_vuelo);
+            fin_carga = true;
+            break;
+         case LLEGO_AVION:
+            Log::info("DespachanteVuelos(%d) llego el avion!!!", num_vuelo);
+            llego_avion = true;
+            break;
+         };
+      }
+      
+      Log::info("DespachanteVuelos(%d) listo para salir, libero zonas y sale el avion (TODO!!!", num_vuelo);
+      // liberar zona (acceso a la BD)
+      // aca le tenemos que avisar al robot_despacho/robot_carga que todos los equipajes estan en camino.x
+
+   } catch (VueloNoRegistradoException) {
+      Log::crit("El vuelo %d no se encuentra registrado en la BD\n", num_vuelo);
+      return 1;
+   }
+}
+
+int get_zona_asignada(int num_vuelo) {
+   if(num_vuelo >=0)
+      return 0;
+   else
+      return 1;
+}
+
+int tomar_zona(int num_zona) {
+   return num_zona;
+}
+
+int get_duracion_checkin(int num_vuelo) {
+   if( num_vuelo>=0 )
+      return 120;
+   else
+      return 125;
+}
