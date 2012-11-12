06caaa77e489be83fb1630ed31d7946d14c97d83
Di Paola Martin <martinp.dipaola@gmail.com>
2012-11-12 16:19:04 -0300

    Por favor, tengan cuidado de no cambiar la identacion de archivos preexistentes, en especial si el archivo esta a cargo de otra persona.

--- ipc/messagequeue.cpp
+++ ipc/messagequeue.cpp
@@ -1,3 +1,4 @@
+
 /*************************************************************************
  *                                                                       *
  *                        This work is licensed under a                  *
@@ -24,6 +25,7 @@
  *                                                                             *
  *******************************************************************************/
 
+
 #include <sys/msg.h>
 #include "oserror.h"
 #include "log.h"
@@ -32,55 +34,62 @@
 #include "commonipc.h"
 
 void MessageQueue::destroy() {
-	Log::debug("%s message queue using the path %s with key %x.", "Destroying", path.c_str(), key);
-	if (msgctl(fd, IPC_RMID, 0) != 0) {
-		throw OSError("The message queue "
-		MESSAGE_Key_Path_Permissions
-		" cannot be destroyed", key, path.c_str(), permissions);
-	}
+   Log::debug("%s message queue using the path %s with key %x.", "Destroying", path.c_str(), key);
+   if(msgctl(fd, IPC_RMID, 0) != 0) {
+      throw OSError("The message queue "
+            MESSAGE_Key_Path_Permissions
+            " cannot be destroyed",
+            key, path.c_str(), permissions);
+   }
 }
 
-MessageQueue::MessageQueue(const char *absolute_path, char proj_id, int permissions, bool create) :
-		owner(create), path(absolute_path), permissions(permissions) {
-	key = get_key(absolute_path, proj_id);
-	Log::debug("%s message queue using the path %s with key %x.", (create ? "Creating" : "Getting"),
-			absolute_path, key);
-	fd = msgget(key, (create ? (IPC_CREAT | IPC_EXCL) : 0) | permissions);
-	if (fd == -1) {
-		throw OSError("The message queue %s "
-		MESSAGE_Key_Path_Permissions, (create ? "cannot be created" : "does not exist"), key,
-				path.c_str(), permissions);
-	}
-}
+MessageQueue::MessageQueue(const char *absolute_path, char proj_id, 
+      int permissions, bool create) : owner(create),
+   path(absolute_path), 
+   permissions(permissions) {
+   key = get_key(absolute_path, proj_id);
+      Log::debug("%s message queue using the path %s with key %x.", (create? "Creating" : "Getting"), absolute_path, key);
+      fd = msgget(key, (create ? (IPC_CREAT | IPC_EXCL) : 0) | permissions);
+      if(fd == -1) {
+         throw OSError("The message queue %s "
+               MESSAGE_Key_Path_Permissions,
+               (create ? "cannot be created" : "does not exist"), 
+               key, path.c_str(), permissions);
+      }
+   }
 
 void MessageQueue::push(const void *msg, size_t size_txt) {
-	if (msgsnd(fd, msg, size_txt, 0) == -1) {
-		throw OSError("The message (text) of %i bytes in the queue "
-		MESSAGE_Key_Path_Permissions
-		" mtype= %d cannot be sent", size_txt, key, path.c_str(), permissions,*((int*)msg));
-	}
+   if(msgsnd(fd, msg, size_txt, 0) == -1) {
+      throw OSError("The message (text) of %i bytes in the queue "
+            MESSAGE_Key_Path_Permissions
+            " cannot be sent", 
+            size_txt, 
+            key, path.c_str(), permissions);
+   }
 }
 
 ssize_t MessageQueue::pull(void *msg, size_t max_size_txt, long type) {
-	ssize_t copyied = msgrcv(fd, msg, max_size_txt, type, 0);
-	if (copyied == -1) {
-		throw OSError("The message (text) of at most %i bytes and type '%i' in the queue "
-		MESSAGE_Key_Path_Permissions
-		" cannot be recieved", max_size_txt, type, key, path.c_str(), permissions);
-	}
-	return copyied;
+   ssize_t copyied = msgrcv(fd, msg, max_size_txt, type, 0);
+   if(copyied == -1) {
+      throw OSError("The message (text) of at most %i bytes and type '%i' in the queue "
+            MESSAGE_Key_Path_Permissions
+            " cannot be recieved", 
+            max_size_txt, type, 
+            key, path.c_str(), permissions);
+   }
+   return copyied;
 }
 
-MessageQueue::~MessageQueue() throw () {
-	if (owner) {
-		Log::debug("%s message queue using the path %s with key %x.", "Destroying", path.c_str(),
-				key);
-		if (msgctl(fd, IPC_RMID, 0) != 0) {
-			Log::crit("An exception happend during the course of a destructor:\n%s",
-					OSError("The message queue "
-					MESSAGE_Key_Path_Permissions
-					" cannot be destroyed", key, path.c_str(), permissions).what());
-		}
-	}
+
+MessageQueue::~MessageQueue() throw() {
+    if(owner) {
+        Log::debug("%s message queue using the path %s with key %x.", "Destroying", path.c_str(), key);
+        if(msgctl(fd, IPC_RMID, 0) != 0) {
+            Log::crit("An exception happend during the course of a destructor:\n%s", OSError("The message queue "
+                        MESSAGE_Key_Path_Permissions
+                        " cannot be destroyed",
+                        key, path.c_str(), permissions).what());
+        }
+    }
 }
 
