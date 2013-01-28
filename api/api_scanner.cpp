88d607d17854e96b0425b7373f28fd651ccb43b3
ferrerogonzalo <ferrerogonzalo@gmail.com>
2013-01-28 19:43:36 -0300

    Test envio mensajes

--- api/api_scanner.cpp
+++ api/api_scanner.cpp
@@ -8,10 +8,8 @@
 ApiScanner::ApiScanner(const char * directorio_de_trabajo, const char* nombre_aeropuerto,
 		int numero_de_sitio, int numero_escaner) :
 		numero_escaner(numero_escaner), cola_grupo_envio(
-				std::string(directorio_de_trabajo).append(PATH_GRUPO_CINTA_CENTRAL).c_str(),
-				0), cola_grupo_recepcion(
-				std::string(directorio_de_trabajo).append(PATH_GRUPO_CINTA_CENTRAL).c_str(),
-				1) {
+				std::string(directorio_de_trabajo).append(PATH_GRUPO_CINTA_CENTRAL).c_str(), 128), cola_grupo_recepcion(
+				std::string(directorio_de_trabajo).append(PATH_GRUPO_CINTA_CENTRAL).c_str(), 0) {
 
 	nombre_aeropuerto = NULL;
 	numero_de_sitio = 0;
@@ -44,10 +42,10 @@ void ApiScanner::colocar_equipaje_en_cinta_principal(const Equipaje & equipaje) 
 Equipaje ApiScanner::extraer_equipaje_de_cinta_escaner() {
 	MENSAJE_ENVIO_GRUPO mensaje;
 
-	cola_grupo_recepcion.pull(&mensaje, sizeof(MENSAJE_ENVIO_GRUPO) - sizeof(long),0);
+	cola_grupo_recepcion.pull(&mensaje, sizeof(MENSAJE_ENVIO_GRUPO) - sizeof(long), 0);
 
-	std::cout <<  "Id: " << mensaje.id_emisor << std::endl;
-	std::cout <<  "Dato: " << mensaje.dato << std::endl;
+	std::cout << "Id: " << mensaje.id_emisor << std::endl;
+	std::cout << "Dato: " << mensaje.dato << std::endl;
 
 	return Equipaje();
 }
